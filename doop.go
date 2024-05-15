35b2e2b0bf2ccf7213ba2d44b5dcf28c12361708
bravian1 <nyatorobravian@gmail.com>
2024-05-15 10:48:34 +0300

    added manual Itoa

--- doop.go
+++ doop.go
@@ -5,8 +5,11 @@ import (
 	"strconv"
 )
 
+//to add Atoi manual
 func doop(s []string) {
-	if len(s[0])>
+	if len(s[0]) > 12 {
+		return
+	}
 	num1, _ := strconv.Atoi(s[0])
 	operator := s[1]
 	num2, _ := strconv.Atoi(s[2])
@@ -45,7 +48,7 @@ func doop(s []string) {
 
 		os.Stdout.Write([]byte(Itoa(result)))
 		os.Stdout.Write([]byte("\n"))
-		
+
 	default:
 		return
 	}
@@ -59,6 +62,29 @@ func main() {
 }
 
 func Itoa(n int) string {
-	//to be done manually
-	return strconv.Itoa(n)
+	//declare variables
+	r := []byte{}
+	temps := []byte{}
+	right := 0
+
+	//handle -ve numbers
+	if n < 0 {
+		r = append(r, '-')
+		n *= -1
+	}
+
+	//convert each digit to a string by adding  "0"
+	for {
+		right = n % 10
+		n = n / 10
+		temps = append(temps, byte('0'+right))
+		if n == 0 {
+			break
+		}
+
+	}
+	for i := len(temps) - 1; i >= 0; i-- {
+		r = append(r, temps[i])
+	}
+	return string(r)
 }
