72f1c0633cbdfcfea002fa1285eeb4c97d2d3734
seodhiambo <oathooh@gmail.com>
2024-05-21 11:26:08 +0300

    chunk update

--- chunk/chunk.go
+++ chunk/chunk.go
@@ -31,35 +31,67 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/01-edu/z01"
+)
 
 func Chunk(slice []int, size int) {
 	if size == 0 {
-        fmt.Println()
-        return
-    }
+		fmt.Println()
+		return
+	}
 	m := len(slice) / size
 	rem := len(slice) % size
-	if rem >= 1  {
+	if rem >= 1 {
 		m++
 	}
 
 	maped := make([][]int, m)
-	
-	n := 0
-	count:=0	
 
-	for i:=0; i<len(slice); i++ {
+	n := 0
+	count := 0
+
+	for i := 0; i < len(slice); i++ {
 		if count == size {
 			n++
-			count=0
+			count = 0
 			i--
 		} else {
-			maped[n] =append(maped[n], slice[i])
+			maped[n] = append(maped[n], slice[i])
 			count++
 		}
 	}
 	println(size, ": ", maped)
+}
+
+func main() {
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+}
+
+// seth solution
+
+func Chunk(slice []int, size int) {
+	if size == 0 {
+		z01.PrintRune('\n')
+		return
+	}
+
+	var newSlice [][]int
+
+	for i := 0; i < len(slice); i += size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
+		}
+		newSlice = append(newSlice, slice[i:end])
+	}
+	fmt.Println(newSlice)
 }
 
 func main() {
